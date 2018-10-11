6a8ea592c5520e33a1528e9d276640da5222efc5
Sven Gehring <cbrxde@gmail.com>
2018-10-11 09:15:06 +0200

    add multi-year gallery option; use new image links

--- assets/script/application.js
+++ assets/script/application.js
@@ -10,11 +10,37 @@ for (var i = 0; i < acrticles.length; i++) {
   menu.appendChild(li);
 }
 
-// copyright year
-var date = document.getElementById('year');
-date.innerText = new Date().getFullYear();
+// gallery years
+$('.sb-picture-year-link').click(function() {
+  var year = $(this).text().trim();
+
+  $('.sb-picture-year-link').removeClass('active');
+  $(this).addClass('active');
+
+  $('#gpfull').animate({ opacity: 0 }, 200, function() {
+    $('#gpfull').attr('href', 'assets/images/'+year+'/group.jpg');
+    $('#gpthumb').attr('src', 'assets/images/'+year+'/group.thumb.jpg');
+    $('#gpfull').animate({ opacity: 1 }, 200);
+  });
+
+  $('.sb-picture-item').each(function(i, pic) {
+    var image = $(this).find('.sb-pic-link')[0];
+    var thumb = $(this).find('.sb-picture-img')[0];
+    var picnr = String(i+1).padStart(2, '0');
+
+    $(image).animate({ opacity: 0 }, 200, function() {
+      $(image).attr('href', 'assets/images/'+year+'/'+picnr+'.jpg');
+      $(thumb).attr('src', 'assets/images/'+year+'/'+picnr+'.thumb.jpg');
+      $(image).animate({ opacity: 1 }, 200);
+    });
+  });
+});
 
 // gallery
 lightbox.option({
   albumLabel: 'Bild %1 von %2'
 })
+
+// copyright year
+var date = document.getElementById('year');
+date.innerText = new Date().getFullYear();
